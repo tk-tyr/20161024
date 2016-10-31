c5a240b55caeb553138a137f00474f732f73c702
50516036 <umasiosan@yahoo.co.jp>
2016-10-31 12:38:04 +0900

    20161024 submit

---
+++ src/calculation/Calculation_main.java
@@ -0,0 +1,22 @@
+package calculation;
+import java.util.Scanner;
+
+public class Calculation_main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner scan = new Scanner(System.in) ;
+		System.out.println("Input Number m> ");
+		int m = Integer.parseInt(scan.next());
+		System.out.println("Input Number n> ");
+		int n = Integer.parseInt(scan.next());
+		
+		Calculation_lib clib = new Calculation_lib(m,n);
+		System.out.println("m + n  = " +clib.getPlus());
+		System.out.println("m - n  = " +clib.getMinus());
+		System.out.println("m * n  = " +clib.getProduct());
+		System.out.println("m / n  = " +clib.getDivid());
+		System.out.println("m % n  = " +clib.getMod());
+	} 
+}
